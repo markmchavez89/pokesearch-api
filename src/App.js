be75dd4590203b4f872516153dc945de4d1309f7
Mark <markmchavez@gmail.com>
2022-01-19 19:15:40 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const[isMouseOver, setMouseOver] = useState('');
+  const[pokemon, setPokemon] = useState('pikachu');
+  const[pokemonData, setPokemonData] = useState([]);
+
+  const getPokemon = async () => {
+    const toArray = [];
+
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      toArray.push(res.data);
+      setPokemonData(toArray);
+      console.log(res)
+    } catch (e) {
+      console.log(e)
+    }
+  };
+  console.log(pokemonData);
+
+  function handleChange(event) {
+    setPokemon(event.target.value.toLowerCase());
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    getPokemon();
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="py-4 container">
+            <div className="row justify-content-center">
+                <div className="col-12 mb-5">
+                    <div className="mb-3 col-4 mx-auto text-center">
+                      <form onSubmit={handleSubmit}>
+                        <label>
+                          <input 
+                          type="text" 
+                          className="form-control"
+                          onChange={handleChange} 
+                          placeholder="Search for a Pokemon..."
+                          style={{display: "inline-block"}}
+                          /><button className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+                        </label>
+                      </form>
+                    </div>
+                </div>
+                
+                {pokemonData.map((data) => {
+                     return (
+                       <div className="col-11 col-md-6 col-lg-4 mx-0 mb-4">
+                        <div className="card p-1 h-100 shadow">
+                            <img src={data.sprites["front_default"]} className="card-image-top" />
+                            <div className="card-body">
+                                <h5 className="card-title" className="text-center">{data.name}</h5>
+                            </div>
+                        </div>
+                    </div>
+                     )
+                }
+                )}
+            </div>
+        </section>
   );
 }
 
